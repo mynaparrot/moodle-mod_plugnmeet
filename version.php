c2a06670114b1d87165cacd277f2f3656d35d77e
jibon Costa <jiboncosta57@gmail.com>
2022-06-18 15:34:23 +0600

    bump version & client

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_plugnmeet';
-$plugin->release = '1.0.3';
-$plugin->version = 2022051203;
+$plugin->release = '1.0.4';
+$plugin->version = 2022061800;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
