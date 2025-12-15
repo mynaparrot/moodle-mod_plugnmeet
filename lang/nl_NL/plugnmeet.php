90f5aa31b76f92c6b8c0a62116ccc6b57ebf14f5
Jibon L. Costa <jiboncosta57@gmail.com>
2025-12-15 08:14:29 +0600

    New translations plugnmeet.php (Dutch) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/nl_NL/plugnmeet.php
+++ lang/nl_NL/plugnmeet.php
@@ -117,7 +117,7 @@ $string['allow_virtual_bg'] = 'Virtuele achtergrond toestaan';
 $string['allow_raise_hand'] = 'Sta hand opsteken toe';
 $string['auto_gen_user_id'] = 'Gebruikers-ID automatisch genereren';
 
-$string['other_features'] = 'Overige kenmerken';
+$string['other_features'] = 'Advanced features';
 
 $string['recordingfeatures'] = 'Opname functies';
 $string['allow_recording'] = 'Opname toestaan';
@@ -219,5 +219,16 @@ $string['allow_speech_to_text_translation_features'] = 'Spraak-naar-tekst/vertaa
 $string['allow_speech_translation'] = 'Vertaling toestaan';
 
 $string['enable_end_to_end_encryption_features'] = 'End-to-end-versleuteling inschakelen (E2EE)';
+$string['enabled_self_insert_encryption_key'] = 'Enable manual E2EE key';
 $string['included_e2ee_chat_messages'] = "Versleuteling inschakelen voor chat";
 $string['included_e2ee_whiteboard'] = "Versleuteling inschakelen voor whiteboard";
+
+$string['insights_features'] = 'Insights AI';
+$string['insights_features_is_allow'] = 'Enable insights features';
+$string['insights_transcription_features_is_allow'] = 'Allow transcription';
+$string['insights_transcription_features_is_allow_translation'] = 'Allow transcription translation';
+$string['insights_transcription_features_is_allow_speech_synthesis'] = 'Allow speech synthesis';
+$string['insights_chat_translation_features_is_allow'] = 'Allow chat translation';
+$string['insights_ai_features_is_allow'] = 'Allow AI features';
+$string['insights_ai_text_chat_features_is_allow'] = 'Allow AI text chat';
+$string['insights_ai_meeting_summarization_features_is_allow'] = 'Allow AI meeting summarization';
