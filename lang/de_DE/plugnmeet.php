485b97e8280e97d46645906b18a1deffd0aaf5f5
Jibon L. Costa <jiboncosta57@gmail.com>
2025-12-15 08:14:27 +0600

    New translations plugnmeet.php (German) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/de_DE/plugnmeet.php
+++ lang/de_DE/plugnmeet.php
@@ -117,7 +117,7 @@ $string['allow_virtual_bg'] = 'Virtuellen Hintergrund zulassen';
 $string['allow_raise_hand'] = 'Erlauben Sie das Heben der Hand';
 $string['auto_gen_user_id'] = 'Benutzer-ID automatisch generieren';
 
-$string['other_features'] = 'Weitere Funktionen';
+$string['other_features'] = 'Advanced features';
 
 $string['recordingfeatures'] = 'Funktionen für die Aufzeichnung';
 $string['allow_recording'] = 'Aufzeichnung zulassen';
@@ -219,5 +219,16 @@ $string['allow_speech_to_text_translation_features'] = 'Sprach-zu-Text-/Überset
 $string['allow_speech_translation'] = 'Übersetzung zulassen';
 
 $string['enable_end_to_end_encryption_features'] = 'Aktivieren der Ende-zu-Ende-Verschlüsselung (E2EE)';
+$string['enabled_self_insert_encryption_key'] = 'Enable manual E2EE key';
 $string['included_e2ee_chat_messages'] = "Aktivieren der Verschlüsselung für den Chat";
 $string['included_e2ee_whiteboard'] = "Aktivieren der Verschlüsselung für Whiteboards";
+
+$string['insights_features'] = 'Insights AI';
+$string['insights_features_is_allow'] = 'Enable insights features';
+$string['insights_transcription_features_is_allow'] = 'Allow transcription';
+$string['insights_transcription_features_is_allow_translation'] = 'Allow transcription translation';
+$string['insights_transcription_features_is_allow_speech_synthesis'] = 'Allow speech synthesis';
+$string['insights_chat_translation_features_is_allow'] = 'Allow chat translation';
+$string['insights_ai_features_is_allow'] = 'Allow AI features';
+$string['insights_ai_text_chat_features_is_allow'] = 'Allow AI text chat';
+$string['insights_ai_meeting_summarization_features_is_allow'] = 'Allow AI meeting summarization';
