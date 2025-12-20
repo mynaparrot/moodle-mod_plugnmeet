1988442e3807c4ec5c0101c2c45645ae1c766b30
Jibon L. Costa <jiboncosta57@gmail.com>
2025-12-20 09:59:59 +0600

    New translations plugnmeet.php (Italian) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/it_IT/plugnmeet.php
+++ lang/it_IT/plugnmeet.php
@@ -117,7 +117,7 @@ $string['allow_virtual_bg'] = 'Consenti sfondo virtuale';
 $string['allow_raise_hand'] = 'Consentire di alzare la mano';
 $string['auto_gen_user_id'] = 'Genera automaticamente l\'ID utente';
 
-$string['other_features'] = 'Advanced features';
+$string['other_features'] = 'Funzionalità avanzate';
 
 $string['recordingfeatures'] = 'Funzioni di registrazione';
 $string['allow_recording'] = 'Consenti registrazione';
@@ -208,8 +208,8 @@ $string['footer_color'] = 'Colore piè di pagina';
 $string['footer_color_des'] = 'Colore del piè di pagina dell\'interfaccia';
 $string['left_color'] = 'Colore lato sinistro';
 $string['left_color_des'] = 'Colore della barra laterale sinistra';
-$string['side_panel_bg_color'] = 'Side Panel background color';
-$string['side_panel_bg_color_des'] = 'Right side panel background color';
+$string['side_panel_bg_color'] = 'Colore di sfondo del pannello laterale';
+$string['side_panel_bg_color_des'] = 'Colore dello sfondo del pannello laterale destro';
 $string['column_camera_position'] = 'Posizione della telecamera a colonna';
 $string['column_camera_position_des'] = 'Posizione della telecamera a colonna. Impostazione predefinita: sinistra';
 $string['column_camera_width'] = 'Larghezza della telecamera a colonna';
@@ -219,16 +219,16 @@ $string['allow_speech_to_text_translation_features'] = 'Consenti funzionalità d
 $string['allow_speech_translation'] = 'Consenti traduzione';
 
 $string['enable_end_to_end_encryption_features'] = 'Abilita crittografia end-to-end (E2EE)';
-$string['enabled_self_insert_encryption_key'] = 'Enable manual E2EE key';
+$string['enabled_self_insert_encryption_key'] = 'Abilita il tasto manuale E2EE';
 $string['included_e2ee_chat_messages'] = "Abilita la crittografia per la chat";
 $string['included_e2ee_whiteboard'] = "Abilita la crittografia per la lavagna";
 
-$string['insights_features'] = 'Insights AI';
-$string['insights_features_is_allow'] = 'Enable insights features';
-$string['insights_transcription_features_is_allow'] = 'Allow transcription';
-$string['insights_transcription_features_is_allow_translation'] = 'Allow transcription translation';
-$string['insights_transcription_features_is_allow_speech_synthesis'] = 'Allow speech synthesis';
-$string['insights_chat_translation_features_is_allow'] = 'Allow chat translation';
-$string['insights_ai_features_is_allow'] = 'Allow AI features';
-$string['insights_ai_text_chat_features_is_allow'] = 'Allow AI text chat';
-$string['insights_ai_meeting_summarization_features_is_allow'] = 'Allow AI meeting summarization';
+$string['insights_features'] = 'Insight AI';
+$string['insights_features_is_allow'] = 'Abilita le funzionalità di insight';
+$string['insights_transcription_features_is_allow'] = 'Consentire la trascrizione';
+$string['insights_transcription_features_is_allow_translation'] = 'Consentire la traduzione della trascrizione';
+$string['insights_transcription_features_is_allow_speech_synthesis'] = 'Consentire la sintesi vocale';
+$string['insights_chat_translation_features_is_allow'] = 'Consentire la traduzione della chat';
+$string['insights_ai_features_is_allow'] = 'Consentire funzionalità di IA';
+$string['insights_ai_text_chat_features_is_allow'] = 'Consenti chat testuale con IA';
+$string['insights_ai_meeting_summarization_features_is_allow'] = 'Permette la sintesi delle riunioni tramite IA';
