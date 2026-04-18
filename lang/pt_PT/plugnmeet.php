4a9529dd693c4960149922c937d94193d8f7c022
Jibon L. Costa <jiboncosta57@gmail.com>
2026-04-18 19:23:03 +0200

    New translations plugnmeet.php (Portuguese) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/pt_PT/plugnmeet.php
+++ lang/pt_PT/plugnmeet.php
@@ -130,7 +130,7 @@ $string['advanced_completion'] = 'Conclusão Avançada';
 $string['available'] = 'Disponível em';
 $string['deadline'] = 'Disponível até';
 $string['err_deadline_before_available'] = 'A data de "Disponível até" não pode ser anterior à data de "Disponível a partir de".';
-$string['error_analytics_required_for_completion'] = 'Enable Analytics must be checked if any completion criteria are set.';
+$string['error_analytics_required_for_completion'] = 'O Enable Analytics deve ser verificado se existem critérios de conclusão definidos.';
 
 $string['allow_webcams'] = 'Permitir Webcams';
 $string['allow_webcams_help'] = 'Permitir que os participantes usem as suas webcams.';
@@ -147,7 +147,7 @@ $string['room_duration_help'] = 'A duração máxima da sala em minutos. 0 sem l
 $string['moderator_join_first'] = 'Moderador Junte-se Primeiro';
 $string['moderator_join_first_help'] = 'Exigir que um moderador se junte antes que outros participantes o façam.';
 $string['enable_analytics'] = 'Ativar Análise';
-$string['enable_analytics_help'] = 'Generate analytics for the session. Note: This must be enabled if any Completion criteria are set.';
+$string['enable_analytics_help'] = 'Gerar análises para a sessão. Nota: Isto deve ser ativado se forem definidos quaisquer critérios de Conclusão.';
 $string['allow_virtual_bg'] = 'Permitir Fundos Virtuais';
 $string['allow_virtual_bg_help'] = 'Permitir que os participantes usem fundos virtuais.';
 $string['auto_gen_user_id'] = 'Gerar Automaticamente ID de Utilizador';
@@ -301,7 +301,7 @@ $string['guest_access_denied'] = 'O acesso de convidados não está ativado para
 $string['invalid_guest_token'] = 'Token de entrada de convidado inválido ou expirado.';
 $string['guest_link_expired'] = 'Este link de entrada de convidados expirou.';
 $string['error_joining_session'] = 'Houve um erro na entrada da sessão: {$a}';
-$string['already_logged_in'] = 'You are already logged into Moodle. You can join the session directly from this activity page.';
+$string['already_logged_in'] = 'Já está ligado ao Moodle. Pode juntar-se diretamente à sessão a partir desta página de atividades.';
 
 // Events
 $string['event_room_started'] = 'A sala começou';
@@ -314,8 +314,8 @@ $string['event_artifact_created'] = 'Artefacto criado';
 $string['event_recording_proceeded'] = 'Gravação pronta';
 
 // Notifications
-$string['messageprovider:recording_ready'] = 'Notifications for ready recordings';
-$string['messageprovider:artifact_ready'] = 'Notifications for ready artifacts';
+$string['messageprovider:recording_ready'] = 'Notificações para gravações prontas';
+$string['messageprovider:artifact_ready'] = 'Notificações para artefactos prontos';
 $string['recording_ready_notification_subject'] = 'Gravação pronta: {$a}';
 $string['recording_ready_notification_fullmessage'] = 'A gravação para a sessão "{$a->roomname}" está agora pronto e processado. Pode vê-lo aqui: {$a->url}';
 $string['recording_ready_notification_fullmessagehtml'] = '<p>A gravação da sessão <strong>"{$a->roomname}"</strong> está agora pronto e processado.</p><p><a href="{$a->url}">Clique aqui para ver a gravação.</a></p>';
