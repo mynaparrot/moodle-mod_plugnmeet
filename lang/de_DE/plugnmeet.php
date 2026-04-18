4dd8e6249902568c99e12d3335009910a1a3435f
Jibon L. Costa <jiboncosta57@gmail.com>
2026-04-18 14:39:27 +0200

    New translations plugnmeet.php (German) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/de_DE/plugnmeet.php
+++ lang/de_DE/plugnmeet.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -300,6 +300,7 @@ $string['guest_access_denied'] = 'Gastzugriff ist für dieses Treffen nicht akti
 $string['invalid_guest_token'] = 'Ungültiges oder abgelaufenes Gast-Join-Token.';
 $string['guest_link_expired'] = 'Dieser Link zum Gastbeitritt ist abgelaufen.';
 $string['error_joining_session'] = 'Es gab einen Fehler beim Einstieg in die Sitzung: {$a}';
+$string['redirect_to_moodle_login'] = 'You are already logged into Moodle. Redirecting you to the activity page...';
 
 // Events
 $string['event_room_started'] = 'Der Raum begann';
