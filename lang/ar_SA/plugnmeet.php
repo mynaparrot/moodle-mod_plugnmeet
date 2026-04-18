09f61062757bd1be6df19fd1bbcd057a7c84fcfd
Jibon L. Costa <jiboncosta57@gmail.com>
2026-04-18 11:59:29 +0200

    New translations plugnmeet.php (Arabic) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/ar_SA/plugnmeet.php
+++ lang/ar_SA/plugnmeet.php
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Strings for component 'mod_plugnmeet', language 'en'
@@ -49,6 +49,11 @@ $string['api_key'] = 'مفتاح API';
 $string['api_key_desc'] = 'مفتاح API الذي يوفره خادم PlugNmeet الخاص بك.';
 $string['api_secret'] = 'سر API';
 $string['api_secret_desc'] = 'سر واجهة برمجة التطبيقات الذي يوفره خادم PlugNmeet الخاص بك.';
+$string['guest_access_hdr'] = 'Guest Access Settings';
+$string['allow_guest_global'] = 'Allow Guest Access Globally';
+$string['allow_guest_global_desc'] = 'If disabled, guest joining cannot be enabled for any meeting room.';
+$string['guest_link_expiration'] = 'Guest Link Expiration (hours)';
+$string['guest_link_expiration_desc'] = 'How long a guest join link remains valid after generation.';
 
 $string['defaults'] = 'الإعدادات الافتراضية التقنية';
 $string['defaults_desc'] = 'الإعدادات التقنية الافتراضية لجلسات PlugNmeet الجديدة.';
@@ -283,6 +288,18 @@ $string['session_available_from'] = 'متوفر من: {$a}';
 $string['session_available_until'] = 'متاح حتى: {$a}';
 $string['session_not_started_yet'] = 'هذه الجلسة لم تبدأ بعد.';
 $string['session_ended'] = 'انتهت هذه الجلسة.';
+$string['allow_guest'] = 'Allow Guest Users';
+$string['allow_guest_help'] = 'Allow external users without a Moodle account to join the session via a shareable link.';
+$string['guest_join_link'] = 'Guest Join Link';
+$string['guest_join_link_help'] = 'Share this link with external participants. This link will remain valid for {$a} hours. Every page reload will generate a new link, but previously shared links will remain valid until their original expiration time.';
+$string['copy_link'] = 'Copy Link';
+$string['guest_join_title'] = 'Join Meeting as Guest';
+$string['enter_display_name'] = 'Enter your display name';
+$string['join_as_guest'] = 'Join Meeting';
+$string['guest_access_denied'] = 'Guest access is not enabled for this meeting.';
+$string['invalid_guest_token'] = 'Invalid or expired guest join token.';
+$string['guest_link_expired'] = 'This guest join link has expired.';
+$string['error_joining_session'] = 'There was an error joining the session: {$a}';
 
 // Events
 $string['event_room_started'] = 'بدأت الغرفة';
