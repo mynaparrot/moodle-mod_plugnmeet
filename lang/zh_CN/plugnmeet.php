5e5216c04e15e87adc7a4b219d97bca93ee16c49
Jibon L. Costa <jiboncosta57@gmail.com>
2026-04-21 23:53:08 +0200

    New translations plugnmeet.php (Chinese Simplified) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/zh_CN/plugnmeet.php
+++ lang/zh_CN/plugnmeet.php
@@ -119,9 +119,12 @@ $string['update_client_btn_label'] = '现在更新';
 $string['yes'] = '是的';
 $string['no'] = '不';
 $string['recordings'] = "录音";
+$string['recording'] = "Recording";
 $string['artifacts'] = "房间伪影";
 $string['attendance'] = "出勤报告";
 $string['artifact'] = "文物";
+$string['room_subject'] = 'room';
+$string['user_subject'] = 'user';
 
 // mod_form.php
 $string['roomtitle'] = '房间标题';
@@ -470,3 +473,19 @@ $string['attendance_mic_duration'] = '麦克风启用时长';
 $string['attendance_whiteboard_annotated'] = '白板注释';
 $string['met'] = '大都会';
 $string['required'] = '必修';
+
+// Status codes
+$string['status_unknown_status'] = 'An unknown status was returned from the server.';
+$string['status_success'] = 'Operation completed successfully.';
+$string['status_permission_denied'] = 'Permission denied.';
+$string['status_not_found'] = 'No {$a} found.';
+$string['status_conflict'] = 'This action could not be completed because of a conflict (e.g., the room may already exist).';
+$string['status_invalid_token_or_signature'] = 'The request token or signature is invalid.';
+$string['status_invalid_api_key'] = 'The API key provided is invalid. Please check your plugin settings.';
+$string['status_internal_server_error'] = 'The PlugNmeet server encountered an internal error.';
+$string['status_invalid_parameters'] = 'The information provided is invalid.';
+$string['status_missing_required_parameter'] = 'Some required information is missing.';
+$string['status_room_not_found'] = 'The specified room was not found.';
+$string['status_user_not_found'] = 'User account not found. Please ensure your API credentials are correct and your subscription is active.';
+$string['status_user_blocked'] = 'User account is blocked. Please contact support.';
+$string['status_user_not_active'] = 'User account is not active. This is likely due to an inactive or expired subscription.';
