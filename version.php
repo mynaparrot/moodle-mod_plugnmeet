b7959d27eb49bd54d8a077e1e108288d59287b9f
Jibon <jiboncosta57@gmail.com>
2026-04-21 11:27:56 +0200

    bump version

--- version.php
+++ version.php
@@ -29,6 +29,6 @@ $plugin->component = 'mod_plugnmeet';
 // x-release-please-start-version
 $plugin->release = '3.0.0';
 // x-release-please-end
-$plugin->version   = 2026042100;
+$plugin->version   = 2026042101;
 $plugin->requires  = 2024051300;
 $plugin->maturity  = MATURITY_STABLE;
