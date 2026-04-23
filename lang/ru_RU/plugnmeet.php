7c116a5dc9179c7fbcdbaec3875b8f9e8071d0a1
Jibon L. Costa <jiboncosta57@gmail.com>
2026-04-23 09:43:08 +0200

    New translations plugnmeet.php (Russian) fix(locale): new translations plugnmeet.php from Crowdin

--- lang/ru_RU/plugnmeet.php
+++ lang/ru_RU/plugnmeet.php
@@ -344,6 +344,10 @@ $string['event_recording_proceeded'] = 'Запись готова';
 $string['event_plugin_error'] = 'Ошибка PlugNmeet';
 $string['event_track_published'] = 'Опубликованный трек';
 $string['event_track_unpublished'] = 'Трек, не опубликованный';
+$string['event_recording_started'] = 'Recording started';
+$string['event_recording_ended'] = 'Recording ended';
+$string['event_rtmp_started'] = 'RTMP stream started';
+$string['event_rtmp_ended'] = 'RTMP stream ended';
 
 // Notifications
 $string['messageprovider:recording_ready'] = 'Уведомления о готовых записях';
